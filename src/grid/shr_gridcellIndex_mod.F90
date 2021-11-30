385943e45c131a5bde514d7ec631f8c0257e3f09
Albert Jornet Puig <jurnix@gmail.com>
2021-11-30 16:55:09 +0100

    Fix: missing class declaration for implemented getIndices subroutine New: implementations for shr_gridcellIndex ==  and toString subroutines

--- src/grid/shr_gridcellIndex_mod.F90
+++ src/grid/shr_gridcellIndex_mod.F90
@@ -15,6 +15,7 @@ module shr_gridcellIndex_mod
   use SHR_error_mod, only: raiseError
   use SHR_precision_mod, only: sp
 
+  use shr_strings_mod, only: string, int2string
   use shr_gridcell_mod, only: shr_gridcell
 
   implicit none
@@ -30,6 +31,12 @@ module shr_gridcellIndex_mod
     integer :: idxlon
   contains
     procedure :: init => gridcellIndex_initialize 
+
+    procedure :: getIndices
+    procedure :: eq_gridcellIndex
+    generic :: operator(==) => eq_gridcellIndex
+
+    procedure :: toString
   end type shr_gridcellIndex
 
 contains
@@ -50,6 +57,28 @@ contains
     indices(1) = self % idxlat
     indices(2) = self % idxlon
   end function getIndices
+
+
+  elemental logical function eq_gridcellIndex(self, other)
+    !< true if 'self' and 'other' have the same attributes
+    class(shr_gridcellIndex), intent(in) :: self
+    type(shr_gridcellIndex), intent(in) :: other 
+    logical :: hasSameIdxLat, hasSameIdxLon
+    hasSameIdxLat = (self % idxlat == other % idxlat)
+    hasSameIdxLon = (self % idxlon == other % idxlon)
+    eq_gridcellIndex = (hasSameIdxLat .and. hasSameIdxLon)
+  end function eq_gridcellIndex
+
+
+  elemental type(string) function toString(self)
+    !< string representation of 'self'
+    class(shr_gridcellIndex), intent(in) :: self
+    type(string) :: latStr, lonStr
+    latStr = int2string(self % idxlat)
+    lonStr = int2string(self % idxlon)
+    toString = string("(idxlat="//latStr % toString()//", idxlon="//&
+            lonStr % toString()//")")
+  end function toString
 
 end module shr_gridcellIndex_mod 
 
