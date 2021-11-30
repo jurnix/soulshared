a22cef596aae9fd47c315329ae11dee54f5930ae
Albert Jornet Puig <jurnix@gmail.com>
2021-11-30 16:55:34 +0100

    New: customized assertTrueAlloc for gridIndicesMapping unit test New: improve unit tests implementation

--- tests/unit/shr_gridIndicesMapping_test.F90
+++ tests/unit/shr_gridIndicesMapping_test.F90
@@ -27,8 +27,9 @@ module shr_gridIndicesMapping_test
 
   type, extends(testSuite) :: testSuitegridIndicesMapping
 
-    contains
-      procedure :: define => defineTestCases
+  contains
+    procedure :: define => defineTestCases
+    procedure :: assertTrueAlloc
   end type 
 
 contains
@@ -43,7 +44,7 @@ contains
     type(shr_gGridAxesBounds) :: latBounds, lonBounds
     type(string) :: latName, lonName
     type(shr_coord) :: topCenter, center, lastGC
-    type(shr_gridcellIndex), allocatable :: foundIndices(:)
+    type(shr_gridcellIndex), allocatable :: foundIndices(:), expIndices(:)
 
     call latBounds % init(1., -1.)
     latname = string("latitude")
@@ -59,29 +60,96 @@ contains
     !
     ! grid
     !
+    ! X -> requested coordinates
+    !
     ! (2) (1) (0)
     !  |   |   |
-    !  +---+---+-  (1.)
-    !  | x | x |
+    !  +---X---X-  (1.)
+    !  |   |   |
     !  +---+---+-  (0.)
-    !  | x | x |
+    !  |   | X |
     !  +---+---+- (-1.)
+    !
+    !  mapped into:
+    !
+    ! +-----+-----+
+    ! | 1,1 | 1,2 |
+    ! +-----+-----+
+    ! | 2,1 | 2,2 |
+    ! *-----+-----+
+    !
     topCenter = shr_coord(1., 1.)
+    allocate(expIndices(2))
+    call expIndices(1) % init(1,1)
+    call expIndices(2) % init(1,2)
     foundIndices = m % getIndex(topCenter)
-    call self % assert(size(foundIndices) == 2, &
-            "l % getIndex(1, 2) size .eq. 2 = T")
+    call self % assertTrueAlloc(foundIndices, expIndices, &
+            "l % getIndex(1, 2) .eq. ([2,1], [2,2]) = T")
+    ! same size?
+!    if (size(foundIndices) /= 2) then ! nope 
+!      call self % assert(.false., &
+!            "l % getIndex(1, 2) size .eq. 2 = T")
+!    else ! yes
+!      ! same elements?
+!      call self % assert(all(foundIndices == expIndices), &
+!            "l % getIndex(1, 2) size .eq. ([2,1], [2,2]) = T")
+!    endif ! (size(foundIndices) /= 2)
+    deallocate(expIndices)
 
 
     center = shr_coord(0., 1.)
+    allocate(expIndices(4))
+    call expIndices(1) % init(1,1)
+    call expIndices(2) % init(1,2)
+    call expIndices(3) % init(2,1)
+    call expIndices(4) % init(2,2)
     foundIndices = m % getIndex(center)
-    call self % assert(size(foundIndices) == 4, &
-            "l % getIndex(0, 1) size .eq. 4 = T")
+!    call self % assert(size(foundIndices) == 4, &
+!            "l % getIndex(0, 1) size .eq. 4 = T")
+    call self % assertTrueAlloc(foundIndices, expIndices, &
+            "l % getIndex(0, 1) size .eq. ([1,1],[1,2],[2,1],[2,2]) = T")
+    deallocate(expIndices)
+
 
     lastGC = shr_coord(-0.5, 0.5) 
+    allocate(expIndices(1))
+    call expIndices(1) % init(2,2)
     foundIndices = m % getIndex(lastGC)
-    call self % assert(size(foundIndices) == 1, &
-            "l % getIndex(-0.5, 0.5) size .eq. 1 = T")
+!    call self % assert(size(foundIndices) == 1, &
+!            "l % getIndex(-0.5, 0.5) size .eq. 1 = T")
+    call self % assertTrueAlloc(foundIndices, expIndices, &
+            "l % getIndex(-0.5, 0.5) size .eq. ([2,2]) = T")
+    deallocate(expIndices)
   end subroutine defineTestCases
+
+
+  subroutine assertTrueAlloc(self, expected, found, textMessage)
+    !< Assertion wrapper to allocatable arrays
+    !< both must be allocated, otherwise false
+    !< both must have the same size, otherwise false
+    class(testSuitegridIndicesMapping), intent(inout) :: self
+    type(shr_gridcellIndex), allocatable, intent(in) :: expected(:)
+    type(shr_gridcellIndex), allocatable, intent(in) :: found(:)
+    character(*), intent(in) :: textMessage
+
+    if (.not. allocated(expected)) then
+      call self % assert(.false., textMessage)
+      return
+    endif
+
+    if (.not. allocated(found)) then
+      call self % assert(.false., textMessage)
+      return
+    endif
+
+    ! same size?
+    if (size(expected) /= size(found)) then ! nope 
+      call self % assert(.false., textMessage)
+    else ! yes
+      ! same elements?
+      call self % assert(all(expected == found), textMessage)
+    endif ! (size(foundIndices) /= 2)
+  end subroutine assertTrueAlloc
 
 end module shr_gridIndicesMapping_test
 
