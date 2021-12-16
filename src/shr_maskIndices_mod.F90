95436ce2466ecd0f43fc105d97c5ae7f55e9f745
Albert Jornet Puig <jurnix@gmail.com>
2021-12-16 11:35:38 +0100

    New: shr_maskIndices_1d how now toString subroutine New: shr_maskIndices_2d how now toString subroutine New: shr_maskIndices_2d is now extended from shr_eqObject_abs Refactor: maskIndices initialization array indices are now redefined

--- src/shr_maskIndices_mod.F90
+++ src/shr_maskIndices_mod.F90
@@ -21,10 +21,10 @@ module shr_maskIndices_mod
 	public :: shr_maskIndices_1d, shr_maskIndices_2d
 
 	!< shr_maskIndices_2d as an array(4)
-	integer, parameter :: MASK_INDICES_ARRAY_ROW_START = 1
-	integer, parameter :: MASK_INDICES_ARRAY_ROW_END= 2
-	integer, parameter :: MASK_INDICES_ARRAY_COL_START = 3
-	integer, parameter :: MASK_INDICES_ARRAY_COL_END= 4
+	integer, parameter :: MASK_INDICES_ARRAY_ROW_START = 3
+	integer, parameter :: MASK_INDICES_ARRAY_ROW_END= 4
+	integer, parameter :: MASK_INDICES_ARRAY_COL_START = 1
+	integer, parameter :: MASK_INDICES_ARRAY_COL_END= 2
 
 
 	!< 1d mask indices
@@ -33,16 +33,18 @@ module shr_maskIndices_mod
 	contains
 		procedure :: init => maskIndices_1d_init
 		procedure :: eq_object => eq_maskIndices_1d
-		procedure :: toString_maskIndices_1d
+		procedure :: toString => toString_maskIndices_1d
 	end type shr_maskIndices_1d
 
 	!< 2d mask indices
-	type shr_maskIndices_2d
+	type, extends(shr_eqObject_abs) :: shr_maskIndices_2d
 		type(shr_maskIndices_1d), allocatable :: row, col
 	contains
 		procedure :: maskIndices_2d_init_by_array
 		procedure :: maskIndices_2d_init_by_1d
 		generic :: init => maskIndices_2d_init_by_array, maskIndices_2d_init_by_1d
+		procedure :: eq_object => eq_maskIndices_2d
+		procedure :: toString => toString_maskIndices_2d
 	end type shr_maskIndices_2d
 
 
@@ -61,7 +63,7 @@ contains
 	subroutine maskIndices_2d_init_by_array(self, array)
 		!< maskIndices_2d constructor with array
 		class(shr_maskIndices_2d), intent(inout) :: self
-		integer, intent(in) :: array(4) !< row start, row end, col start, col end
+		integer, intent(in) :: array(4) !< col start, col end, row start, row end,
 
 		type(shr_maskIndices_1d) :: colIndices, rowIndices
 		call colIndices % init(array(MASK_INDICES_ARRAY_COL_START), &
@@ -106,15 +108,46 @@ contains
 	end function eq_maskIndices_1d
 
 
-	type(string) function toString_maskIndices_1d(self) result (s)
+	elemental type(string) function toString_maskIndices_1d(self) result (s)
 		!< string representation of maskIndices_1d
 		clasS(shr_maskIndices_1d), intent(in) :: self
-		character(:), allocatable :: str
 		type(string) :: strStart, strEnd
 
 		strStart = int2string(self % start)
 		strEnd = int2string(self % end)
 		s = string("(") + self % start + ":" + self % end + ")"
 	end function toString_maskIndices_1d
+
+
+	elemental logical function eq_maskIndices_2d(self, other)
+		!< true if 'self' and 'other' have the same attributes
+		class(shr_maskIndices_2d), intent(in) :: self
+		class(shr_eqObject_abs), intent(in) :: other
+		logical :: hasSameCol, hasSameRow
+		logical :: hasSameType
+
+		select type(o => other)
+		type is(shr_maskIndices_2d)
+			hasSameType = .true.
+			hasSameCol = (self % col == o % col)
+			hasSameRow = (self % row == o % row)
+		class default
+			hasSameType = .false.
+			hasSameCol = .false.
+			hasSameRow = .false.
+		end select
+
+		eq_maskIndices_2d = (hasSameType .and. hasSameCol .and. hasSameRow)
+	end function eq_maskIndices_2d
+
+
+	elemental type(string) function toString_maskIndices_2d(self) result (s)
+		!< string representation of maskIndices_2d
+		class(shr_maskIndices_2d), intent(in) :: self
+		type(string) :: strCol, strRow
+		strCol = self % col % toString()
+		strRow = self % row % toString()
+		s = string("[") + strCol + ", " + strRow + "]"
+	end function toString_maskIndices_2d
 
 end module shr_maskIndices_mod
