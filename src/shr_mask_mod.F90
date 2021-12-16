b8e3e6aecb8a1364f27d1b24aa5054691dce128c
Albert Jornet Puig <jurnix@gmail.com>
2021-12-16 18:31:59 +0100

    New: implement missing procedures from shr_mask2d

--- src/shr_mask_mod.F90
+++ src/shr_mask_mod.F90
@@ -42,23 +42,28 @@ module shr_mask_mod
 	type :: shr_mask2d
 		logical, allocatable :: lmask(:,:)
 	contains
-!		procedure :: init => mask2d_initialize
+		procedure :: mask2d_initialize_bySize!(4)
+		procedure :: mask2d_initialize_byArray!(4)
+		generic :: init =>  mask2d_initialize_bySize, mask2d_initialize_byArray
+		procedure :: getSize => mask2d_getSize
+
 		procedure :: get => mask2d_get
+		procedure :: filter => mask2d_filter !< new mask with selected indices
 	end type shr_mask2d
 
 contains
 
-	subroutine mask1d_initialize_bySize(self, size, default)
+	subroutine mask1d_initialize_bySize(self, dim1, default)
 		!< initialize with new size
 		!< By default true is assigned,
 		class(shr_mask1d), intent(inout) :: self
-		integer, intent(in) :: size
+		integer, intent(in) :: dim1
 		logical, intent(in), optional :: default
 		logical :: inDefault
-		allocate(self % lmask(size))
 
 		inDefault = .true.
 		if (present(default)) inDefault = default
+		allocate(self % lmask(dim1))
 		self % lmask = inDefault
 	end subroutine mask1d_initialize_bySize
 
@@ -110,5 +115,57 @@ contains
 		allocate(m, source = self % lmask)
 	end function mask2d_get
 
+	subroutine mask2d_initialize_bySize(self, dim1, dim2, default)
+		!< initialize with new size
+		!< By default true is assigned,
+		class(shr_mask2d), intent(inout) :: self
+		integer, intent(in) :: dim1, dim2
+		logical, intent(in), optional :: default
+		logical :: inDefault
+
+		inDefault = .true.
+		if (present(default)) inDefault = default
+		allocate(self % lmask(dim1, dim2))
+		self % lmask = inDefault
+	end subroutine mask2d_initialize_bySize
+
+
+	subroutine mask2d_initialize_byArray(self, larray)
+		!< initialize with new size with logical array
+		class(shr_mask2d), intent(inout) :: self
+		logical, intent(in) :: larray(:,:)
+		allocate(self % lmask, source =  larray)
+	end subroutine mask2d_initialize_byArray
+
+
+	type(shr_mask2d) function mask2d_filter(self, mIndices) result (m)
+		!< filter those mask values found in mIndices in a new shr_mask
+		class(shr_mask2d), intent(inout) :: self
+		type(shr_maskIndices_2d), intent(in) :: mIndices
+
+		integer :: startCol, endCol
+		integer :: startRow, endRow
+		integer, allocatable :: sz(:)
+		type(shr_maskIndices_1d) :: rowIdx, colIdx
+
+		!< init as empty
+		sz = shape( self % lmask )
+		call m % init(sz(1), sz(2), .false.)
+		rowIdx = mIndices % getRow()
+		colIdx = mIndices % getCol()
+		startCol = colIdx % start !< getStart
+		endCol = colIdx % end !< getEnd
+		startRow = rowIdx % start
+		endRow = rowIdx % end
+		!< copy values (get(mindices, values), set(mindices, values))
+		m % lmask(startRow:endRow, startCol:endCol) = self % lmask(startRow:endRow, startCol:endCol)
+	end function mask2d_filter
+
+
+	integer function mask2d_getSize(self)
+		!< get mask size
+		class(shr_mask2d), intent(in) :: self
+		mask2d_getSize = size(self % lmask)
+	end function mask2d_getSize
 
 end module shr_mask_mod
