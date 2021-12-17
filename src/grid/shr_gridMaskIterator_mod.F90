990e7317f9e08aec31a176adeeeaa72d853914aa
Albert Jornet Puig <jurnix@gmail.com>
2021-12-17 14:19:17 +0100

    New: shr_gridMaskIterator_mod.F90 include class attributes New: shr_gridMaskIterator_cast subroutine to change type Refactor: shr_gridMaskIterator rename from get to getNext

--- src/grid/shr_gridMaskIterator_mod.F90
+++ src/grid/shr_gridMaskIterator_mod.F90
@@ -9,7 +9,10 @@
 ! DESCRIPTION:
 !>
 !> Iterates shr_gridMask block by block
-!> At each step it returns itself with only enabled cells
+!>
+!> At each step it returns:
+!> - same grid descriptor
+!> - subset of enabled cells
 !>
 !------------------------------------------------------------------------------
 module shr_gridMaskIterator_mod
@@ -21,7 +24,7 @@ module shr_gridMaskIterator_mod
 
   implicit none
 
-  public :: shr_gridMaskIterator
+  public :: shr_gridMaskIterator, shr_gridMaskIterator_cast
 
   logical, parameter :: ISDEBUG = .false.
 
@@ -29,10 +32,13 @@ module shr_gridMaskIterator_mod
   type, extends(shr_iterator_abs) :: shr_gridMaskIterator
     !< mask
     type(shr_gridMask), allocatable :: gMask
+    type(shr_gridMask), allocatable :: maskedGroups
+    integer :: counter
+    integer :: total
   contains
     procedure :: init => gridMaskIterator_initialize
     procedure :: hasNext
-    procedure :: get
+    procedure :: getNext
   end type shr_gridMaskIterator
 
 contains
@@ -42,20 +48,47 @@ contains
     class(shr_gridMaskIterator), intent(inout) :: self
     type(shr_gridMask), intent(in) :: gridMask
 
+    if (allocated(self % gMask)) deallocate(self % gMask) !< enable resuse of the same object
+    allocate(self % gMask, source = gridMask)
+    !call maskedGrouped % init(self % gMask)
+    !self % total = maskedGrouped % size()
+    !maskedGrouped % get()
+    self % counter = 0
   end subroutine gridMaskIterator_initialize
 
 
   logical function hasNext(self)
     !< true if there is another field to iterate
     class(shr_gridMaskIterator), intent(in) :: self
+    hasNext = (self % total > self % counter)
   end function hasNext
 
 
-  function get(self) result (obj)
+  function getNext(self) result (obj)
     !< returns current object
-    class(shr_gridMaskIterator), intent(in) :: self
+    class(shr_gridMaskIterator), intent(inout) :: self
     class(*), allocatable :: obj
-  end function get
+    if (self % hasNext()) then
+      self % counter = self % counter + 1
+      !allocate(obj, source = maskedGrouped % get(self % counter))
+    end if
+  end function getNext
+
+
+  subroutine shr_gridMaskIterator_cast(obj, gMIterator)
+    !< Cast from * to shr_gridMaskIterator
+    class(*), intent(in) :: obj
+    type(shr_gridMaskIterator), intent(out) :: gMIterator
+
+    select type(o => obj)
+    type is(shr_gridMaskIterator)
+      gMIterator = o
+    class default
+      call raiseError(__FILE__, "shr_gridMaskIterator_cast", &
+          "Unexpected type found instead of 'shr_gridMaskIterator'")
+    end select
+
+  end subroutine shr_gridMaskIterator_cast
 
 end module shr_gridMaskIterator_mod
 
