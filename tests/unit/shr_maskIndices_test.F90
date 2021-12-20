6bfd81c7e46fb6ed9523f7f699743ae97573bf0d
Albert Jornet Puig <jurnix@gmail.com>
2021-12-20 10:33:34 +0100

    New: shr_maskIndices_test.F90 missing unit tests for shr_maskIndices_2d

--- tests/unit/shr_maskIndices_test.F90
+++ tests/unit/shr_maskIndices_test.F90
@@ -61,15 +61,54 @@ contains
   subroutine testMaskIndices2d(self)
     !<
     class(testSuiteMaskIndices), intent(inout) :: self
-    call self % assert(.false., "todo = T")
+    type(shr_maskIndices_2d) :: m2, mother
+    type(shr_maskIndices_1d) :: cols, rows
 
     !procedure :: maskIndices_2d_init_by_array
+    call m2 % init([3,0,4,1])
+    call self % assert(.true., "m2 % init(3,0,4,1) = T")
+
     !procedure :: maskIndices_2d_init_by_1d
+    call cols % init(3,0)
+    call cols % init(4,1)
+    call m2 % init(cols, rows)
+    call self % assert(.true., "m2 % init(cols, rows) = T")
+
     !procedure :: eq_object => eq_maskIndices_2d
+    !< same variable
+    call m2 % init([3,0,4,1])
+    call self % assert(m2 == m2, "m2 .eq. m2 = T")
+
+    ! same attributes but different variables
+    call m2 % init([3,0,4,1])
+    call cols % init(3,0)
+    call rows % init(4,1)
+    call mother % init(cols, rows)
+    call self % assert(m2 == mother, "m2(3,0,4,1) .eq. mOther(cols(3,0), rows(4,1)) = T")
+
+    !< different attributes
+    call m2 % init([3,0,4,1])
+    call mother % init([3,1,4,1])
+    call self % assert(.not. (m2 == mother), "m2(3,0,4,1) .eq. mOther(3,1,4,1) = T")
+
     !procedure :: toString => toString_maskIndices_2d
+    call m2 % init([3,0,4,1])
+    call self % assert(m2 % toString() == "[(3:0), (4:1)]", &
+            "m2(3,0,4,1) % toString() = T")
 
     !procedure :: getRow
+    call cols % init(3,0)
+    call rows % init(4,1)
+    call mother % init(cols, rows)
+    call self % assert(mother % getRow() == rows, &
+        "mOther(3,0,4,1) % getRow() .eq. rows(4,1) = T")
+
     !procedure :: getCol
+    call cols % init(3,0)
+    call rows % init(4,1)
+    call mother % init(cols, rows)
+    call self % assert(mother % getCol() == cols, &
+        "mOther(3,0,4,1) % getRow() .eq. cols(3,0) = T")
   end subroutine testMaskIndices2d
 
 end module shr_maskIndices_test
