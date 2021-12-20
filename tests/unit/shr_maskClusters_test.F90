c3f8659893e9e4ad4a6a1fcb15bcbfc3e91eed3e
Albert Jornet Puig <jurnix@gmail.com>
2021-12-20 11:52:57 +0100

    Refactor: shr_maskClusters_test.F90 unit tests are now divided into 2 subroutines to differentiate 1d and 2d

--- tests/unit/shr_maskClusters_test.F90
+++ tests/unit/shr_maskClusters_test.F90
@@ -28,27 +28,32 @@ module shr_maskClusters_test
 
   contains
     procedure :: define => defineTestCases
+    procedure, private :: testMaskClusters1d
+    procedure, private :: testMaskClusters2d
   end type
 contains
 
   subroutine defineTestCases(self)
     use iso_c_binding
     class(testSuiteMaskClusters), intent(inout) :: self
-    logical :: mask2d(4, 6)
+
+    call self % testMaskClusters1d()
+    call self % testMaskClusters2d()
+  end subroutine defineTestCases
+
+
+  subroutine testMaskClusters1d(self)
+    !< tests for shr_maskcluster_1d
+    class(testSuiteMaskClusters), intent(inout) :: self
+
     logical :: mask1d(6)
-    type(shr_maskIndices_1d), allocatable :: mIndices(:)
     type(shr_maskIndices_1d) :: expIndices(2)
-
-    type(shr_maskIndices_2d), allocatable :: mMatrixIndices(:)
-    type(shr_maskIndices_2d) :: expMatrixIndices(5)
-
-    integer :: istr
-    type(string), allocatable :: tmpStrs(:)
+    type(shr_maskIndices_1d), allocatable :: mIndices(:)
 
     !< 1d mask
     !write(*,*) shr_mask_calc_SquaredGroups([.true., .false.])
     call self % assert(shr_mask_count_groups_l1([.true., .false.]) == 1, &
-            "shr_mask_count_groups_l1(T, F) .eq. 1 = T")
+        "shr_mask_count_groups_l1(T, F) .eq. 1 = T")
 
     !write(*,*) shr_mask_calc_SquaredGroups([.true., .false., .true., .false.])
     call self % assert(shr_mask_count_groups_l1([.true., .false., .true., .false.]) == 2, &
@@ -61,6 +66,37 @@ contains
     !write(*,*) shr_mask_calc_SquaredGroups([.true., .true., .true., .false., .true.])
     call self % assert(shr_mask_count_groups_l1([.true., .true., .true., .false., .true.]) == 2, &
         "shr_mask_count_groups_l1(T, T, T, F, T) .eq. 2 = T")
+
+
+    !< 1d calc mask indices
+    !< T T F F T T = 2
+    mask1d = [.true., .true., .false., .false., .true. ,.true.]
+    call expIndices(1) % init(1,2)
+    call expIndices(2) % init(5,6)
+    mIndices = shr_mask_calc_groups_indices_l1(mask1d)
+    call self % assert(all(mIndices == expIndices), &
+        "shr_mask_calc_groups_indices_l1(TTFFTT) .eq. [(1,2),(5,6)] = T")
+
+    !< T T F F F T = 2
+    mask1d = [.true., .true., .false., .false., .false. ,.true.]
+    call expIndices(1) % init(1,2)
+    call expIndices(2) % init(6,6)
+    mIndices = shr_mask_calc_groups_indices_l1(mask1d)
+    call self % assert(all(mIndices == expIndices), &
+        "shr_mask_calc_groups_indices_l1(TTFFTT) .eq. [(1,2),(6,6)] = T")
+  end subroutine testMaskClusters1d
+
+
+  subroutine testMaskClusters2d(self)
+    !<
+    class(testSuiteMaskClusters), intent(inout) :: self
+    logical :: mask2d(4, 6)
+
+    type(shr_maskIndices_2d), allocatable :: mMatrixIndices(:)
+    type(shr_maskIndices_2d) :: expMatrixIndices(5)
+
+    integer :: istr
+    type(string), allocatable :: tmpStrs(:)
 
     !< 2d mask
     !< (horizontal)
@@ -87,23 +123,6 @@ contains
     call self % assert(shr_mask_count_groups_l2(mask2d) == 6, &
         "shr_mask_count_groups_l2(...) .eq. 6 = T")
 
-    !< 1d calc mask indices
-    !< T T F F T T = 2
-    mask1d = [.true., .true., .false., .false., .true. ,.true.]
-    call expIndices(1) % init(1,2)
-    call expIndices(2) % init(5,6)
-    mIndices = shr_mask_calc_groups_indices_l1(mask1d)
-    call self % assert(all(mIndices == expIndices), &
-        "shr_mask_calc_groups_indices_l1(TTFFTT) .eq. [(1,2),(5,6)] = T")
-
-    !< T T F F F T = 2
-    mask1d = [.true., .true., .false., .false., .false. ,.true.]
-    call expIndices(1) % init(1,2)
-    call expIndices(2) % init(6,6)
-    mIndices = shr_mask_calc_groups_indices_l1(mask1d)
-    call self % assert(all(mIndices == expIndices), &
-        "shr_mask_calc_groups_indices_l1(TTFFTT) .eq. [(1,2),(6,6)] = T")
-
     !< 2d calc mask indices
     !< (horizontal)
     !< T T F F T T -> 2
@@ -122,23 +141,23 @@ contains
     mMatrixIndices = shr_mask_calc_groups_indices_l2(mask2d)
 
     call self % assert(size(mMatrixIndices) == 5, &
-    "shr_mask_calc_groups_indices_l2(TTFFTT,T,T,T,FFFFT) size .eq. 5 = T")
+        "shr_mask_calc_groups_indices_l2(TTFFTT,T,T,T,FFFFT) size .eq. 5 = T")
 
-    allocate(tmpStrs(size(mMatrixIndices)))
-    do istr = 1, size(mMatrixIndices) ! debug only
+!    allocate(tmpStrs(size(mMatrixIndices)))
+!    do istr = 1, size(mMatrixIndices) ! debug only
       !write(*,*) "col allocated? ", allocated(mMatrixIndices(istr) % col)
       !write(*,*) "row allocated? ", allocated(mMatrixIndices(istr) % row)
       !write(*,*) "col: ", mMatrixIndices(istr) % col % start, mMatrixIndices(istr) % col % end
       !write(*,*) "row: ", mMatrixIndices(istr) % row % start, mMatrixIndices(istr) % row % end
-      tmpStrs = mMatrixIndices(istr) % toString()
-      write(*,*) "MatrixIndices: ", tmpStrs(1) % toString()
-      tmpStrs = expMatrixIndices(istr) % toString()
-      write(*,*) "Expected: ", tmpStrs(1) % toString()
-      tmpStrs = mMatrixIndices(istr) % toString()
-      write(*,*) "same? ", mMatrixIndices(istr) == expMatrixIndices(istr)
-    end do
+!      tmpStrs = mMatrixIndices(istr) % toString()
+!      write(*,*) "MatrixIndices: ", tmpStrs(1) % toString()
+!      tmpStrs = expMatrixIndices(istr) % toString()
+!      write(*,*) "Expected: ", tmpStrs(1) % toString()
+!      tmpStrs = mMatrixIndices(istr) % toString()
+!      write(*,*) "same? ", mMatrixIndices(istr) == expMatrixIndices(istr)
+!    end do
     call self % assert(all(mMatrixIndices == expMatrixIndices), &
-          "shr_mask_calc_groups_indices_l2(TTFFTT,T,T,T,FFFFT) .eq. (...) = T")
-  end subroutine defineTestCases
+        "shr_mask_calc_groups_indices_l2(TTFFTT,T,T,T,FFFFT) .eq. (...) = T")
+  end subroutine testMaskClusters2d
 
 end module shr_maskClusters_test
