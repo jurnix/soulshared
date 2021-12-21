ad781ffd277c6dce7d6943b4f0d718abcac73c78
Albert Jornet Puig <jurnix@gmail.com>
2021-12-21 15:32:57 +0100

    New: shr_gridMaskClusters is now properly initialized

--- src/grid/shr_gridMaskClusters_mod.F90
+++ src/grid/shr_gridMaskClusters_mod.F90
@@ -8,15 +8,7 @@
 !
 ! DESCRIPTION:
 !>
-!> Find clusters in a shr_gridMask
-!>
-!> clusters are composed of:
-!>  - squared subgrid parts
-!>  - enabled gridcells only
-!>
-!> found clusters are returned as:
-!>  - same grid
-!>  - only enabled those gridcells included in the cluster
+!> shr_gridMask wrapper to shr_maskClusters_2d
 !>
 !------------------------------------------------------------------------------
 module shr_gridMaskClusters_mod
@@ -24,7 +16,9 @@ module shr_gridMaskClusters_mod
   !use SHR_precision_mod, only: sp
 
   use shr_gridMask_mod, only: shr_gridMask
+  use shr_gGridDescriptor_mod, only: shr_gGridDescriptor
   use shr_maskClusters_mod, only: shr_maskClusters_2d
+  use shr_mask_mod, only: shr_mask2d
 
   implicit none
 
@@ -42,10 +36,6 @@ module shr_gridMaskClusters_mod
     procedure :: init
     procedure :: getSize
     procedure :: get
-
-    procedure, private :: calculate
-    procedure, private :: calculateTotal
-
   end type shr_gridMaskClusters
 
 contains
@@ -54,10 +44,32 @@ contains
     !< gridMask initialization
     class(shr_gridMaskClusters), intent(inout) :: self
     type(shr_gridMask), intent(in) :: gridMask
+    type(shr_mask2d) :: mask
+    logical, allocatable :: lmask(:,:)
+    integer :: nclusters, icluster
+    type(shr_mask2d) :: tmpMaskCluster
+    type(shr_gGridDescriptor) :: gDescriptor
+
     allocate(self % mask, source = gridMask)
     allocate(self % clusters)
 
-    !< build here
+    !< initialize each cluster to shr_gridMask
+    !< from shr_gridMask to shr_maskClusters
+    lmask = self % mask % getRaw()
+    call mask % init(lmask)
+    call self % clusters % init(mask)
+
+
+    nclusters = self % clusters % getSize()
+    allocate(self % groups(nclusters))
+
+    gDescriptor = self % mask % getGridDescriptor()
+    !< for each cluster
+    do icluster = 1, nclusters !< cluster found
+      tmpMaskCluster = self % clusters % get(icluster)
+      lmask = tmpMaskCluster % get()
+      call self % groups(icluster) % init(gDescriptor, lmask)
+    end do
   end subroutine init
 
 
@@ -93,7 +105,6 @@ contains
     end if
     get = self % groups(pos)
   end function get
-
 
 end module shr_gridMaskClusters_mod
 
