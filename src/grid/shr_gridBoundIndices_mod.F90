a2dcfd4a30fa6b98f7971b4ee878ca86d186dcfe
Albert Jornet Puig <jurnix@gmail.com>
2021-12-30 14:09:12 +0100

    New: shr_gridBoundIndices_mod.F90 file

---
+++ src/grid/shr_gridBoundIndices_mod.F90
@@ -0,0 +1,99 @@
+!------------------------------------------------------------------------------
+!    Pekin University - Land Surface Model
+!------------------------------------------------------------------------------
+! MODULE        : shr_gridBoundIndices_mod
+!
+!> @author
+!> Albert Jornet Puig
+!
+! DESCRIPTION:
+!> grid bounds indices which defines a grid
+!------------------------------------------------------------------------------
+
+module shr_gridBoundIndices_mod
+
+  use SHR_precision_mod, only: sp
+  use SHR_error_mod, only: raiseError
+  use shr_gGridAxesBounds_mod, only: shr_gGridAxesBounds
+
+  implicit none
+
+  public :: shr_gridBoundIndices
+
+  private 
+
+  logical, parameter :: ISDEBUG = .false.
+
+
+  !<
+  type shr_gridBoundIndices ! coordinates to select a part of the world
+    integer :: startRow, endRow
+    integer :: startCol, endCol
+  contains
+    procedure :: init
+
+    procedure :: getStartRow
+    procedure :: getEndRow
+    procedure :: getStartCol
+    procedure :: getEndCol
+
+    procedure :: countRows
+    procedure :: countCols
+  end type shr_gridBoundIndices
+
+contains
+
+  subroutine init(self, startRow, endRow, startCol, endCol)
+    !< initialize
+    class(shr_gridBoundIndices), intent(inout) :: self
+    integer, intent(in) :: startRow, endRow, startCol, endCol
+    self % startRow = startRow
+    self % endRow = endRow
+    self % startCol = startCol
+    self % endCol = endCol
+  end subroutine init
+
+
+  integer function getStartRow(self) result (idx)
+    !< start row index
+    class(shr_gridBoundIndices), intent(in) :: self
+    idx = self % startRow
+  end function getStartRow
+
+
+  integer function getEndRow(self) result (idx)
+    !< end row index
+    class(shr_gridBoundIndices), intent(in) :: self
+    idx = self % endRow
+  end function getEndRow
+
+
+  integer function getStartCol(self) result (idx)
+    !< start col index
+    class(shr_gridBoundIndices), intent(in) :: self
+    idx = self % startCol
+  end function getStartCol
+
+
+  integer function getEndCol(self) result (idx)
+    !< end col index
+    class(shr_gridBoundIndices), intent(in) :: self
+    idx = self % endCol
+  end function getEndCol
+
+
+  integer function countRows(self) result (nrows)
+    !< total number of rows
+    class(shr_gridBoundIndices), intent(in) :: self
+    nrows = self % endRow - self % startRow + 1
+  end function countRows
+
+
+  integer function countCols(self) result (ncols)
+    !< total number of rows
+    class(shr_gridBoundIndices), intent(in) :: self
+    ncols = self % endCol - self % startCol + 1
+  end function countCols
+
+end module shr_gridBoundIndices_mod
+
