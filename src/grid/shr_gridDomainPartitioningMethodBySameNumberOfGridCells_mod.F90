f299fe74ff2799afc642761c6a6a9a69b7ecfebb
ajornet <jurnix@gmail.com>
2022-02-10 17:35:51 +0100

    New: implementation for shr_gridDomainPartitioningMethodBySameNumberOfGridCells_mod.F90 Clean: improve documentation

--- src/grid/shr_gridDomainPartitioningMethodBySameNumberOfGridCells_mod.F90
+++ src/grid/shr_gridDomainPartitioningMethodBySameNumberOfGridCells_mod.F90
@@ -8,49 +8,15 @@
 !
 ! DESCRIPTION:
 !>
-!> shr_gridDomain is partitioned into the same number of consecutive gridcells
-!> x x x x (3)
-!> x x x x --->  4x4 positions to divide into 3
-!> x x x x
-!> x x x x
-!> 
-!> 1st
-!> x x x x
-!> x b b b
-!> 
-!> 2nd
-!> b x x x
-!> x x b b
-!> 
-!> 3rd
-!> 
-!> b b x x
-!> x x x x
-!> 
-!> ---------------------------------------------------------------------
-!> 
-!> x x x x (3)
-!> x - x x --->  14 positions to divide into 3
-!> x x - x
-!> x x x x
-!> 
-!> 
-!> gridDomain
-!> 1st
-!> 
-!> 
-!> x x x x   -> gridmask	(x x x x)
-!> 
-!> 2nd
-!> 
-!> x - x x   -> gridmask	(x - x x)
-!> x x b b		(x x    )
-!> 
-!> 3rd
-!> 
-!> b b - x   -> gridmask	(    - x)
-!> x x x x		(x x x x)
+!> shr_gridDomain is partitioned into the same number of
+!> enabledhorizontal consecutive gridcells.
 !>
+!> 8 enabled gridcells into 3 parts:
+!>            (domain 1)  (domain 2)    (domain 3)
+!> x x - x     x x - x
+!> x - - x  ->          -> x - - x  ->
+!> x x - -                 x b b b      b x - -
+!> - - x -                              - - x -
 !>
 !------------------------------------------------------------------------------
 module shr_gridDomainPartitioningMethodBySameNumberOfGridcells_mod
@@ -59,8 +25,12 @@ module shr_gridDomainPartitioningMethodBySameNumberOfGridcells_mod
 
   use shr_gridDomainPartitioningMethod_abs, only: shr_gridDomainPartitioningMethod
 
-  use shr_gridMask_mod, only: shr_gridMask_cast, shr_IgridMask
+  use shr_gGrid_mod, only: shr_igGrid
+  use shr_gridMask_mod, only: shr_gridMask_cast, shr_IgridMask, shr_gridMask
   use shr_gridDomain_mod, only: shr_gridDomain, shr_iGridDomain
+
+  use shr_gridMaskFindEnabledEqualSplitIterator_mod, only: shr_gridMaskFindEnabledEqualSplitIterator
+  use shr_gridMaskFindEnabledEqualSplitMethod_mod, only: shr_gridMaskFindEnabledEqualSplitMethod
 
 
   implicit none
@@ -77,8 +47,8 @@ module shr_gridDomainPartitioningMethodBySameNumberOfGridcells_mod
     class(shr_igridDomain), allocatable :: gdomain !< input
     class(shr_igridDomain), allocatable :: newDomains(:) !< output
 
-    !< arguments
-    integer, allocatable :: current, total
+    type(shr_gridMaskFindEnabledEqualSplitMethod), allocatable :: findEnabledEqualSplitMethod
+    integer, allocatable :: total
   contains
     procedure :: init
     procedure :: calculate
@@ -87,13 +57,12 @@ module shr_gridDomainPartitioningMethodBySameNumberOfGridcells_mod
 
 contains
 
-  subroutine init(self, current, total)
+  subroutine init(self, total)
     !< initialize with custom arguments
     class(shr_gridDomainPartitioningMethodBySameNumberOfGridcells), intent(inout) :: self
-    integer, intent(in) :: current, total
+    integer, intent(in) :: total
 
-    allocate(self % current, self % total)
-    self % current = current
+    allocate( self % total)
     self % total = total
   end subroutine init
 
@@ -102,13 +71,28 @@ contains
     !< compute partitions
     class(shr_gridDomainPartitioningMethodBySameNumberOfGridcells), intent(inout) :: self
     class(shr_iGridDomain), intent(in) :: domain
-    class(shr_IgridMask), allocatable :: emask
+    class(shr_IgridMask), allocatable :: enabled
+    class(shr_iGridDomain), allocatable :: filteredDomain
+    class(shr_iggrid), allocatable :: filteredGrid
+    integer :: isplit
+    type(shr_gridMask) :: tmpGMask
+
     allocate(self % gdomain, source = domain)
+    allocate(self % newDomains(self % total), mold = domain)
+    allocate(self % findEnabledEqualSplitMethod)
 
-    !emask = self % gdomain % getEnabledGridMask()
-    !call gridMaskSplit % init(emask, method=equal)
-    !call gridMaskSplit % calculate()
+    !< calculate earch part
+    enabled = self % gdomain % getEnabledGridMask()
+    call self % findEnabledEqualSplitMethod % init(self % total, enabled)
 
+    !< over each part
+    do isplit = 1, self % findEnabledEqualSplitMethod % getSize()
+      !> convert each gridMask into new domain
+      tmpGMask = self % findEnabledEqualSplitMethod % get(isplit)
+      filteredDomain = self % gdomain % filter(tmpGMask)
+      filteredGrid = filteredDomain % getGrid()
+      self % newDomains(isplit) = self % gdomain % select(filteredGrid)
+    end do
   end subroutine calculate
 
 
