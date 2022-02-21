a63daf93e18d49d4676aa0319b4924543e5da812
ajornet <jurnix@gmail.com>
2022-02-21 16:48:29 +0100

    Fix: properly extract from grid domain each part

--- src/grid/shr_gridDomainPartitioningMethodBySameNumberOfGridCells_mod.F90
+++ src/grid/shr_gridDomainPartitioningMethodBySameNumberOfGridCells_mod.F90
@@ -22,12 +22,13 @@
 module shr_gridDomainPartitioningMethodBySameNumberOfGridcells_mod
 
   use SHR_error_mod, only: raiseError
+  use shr_strings_mod, only: string
 
   use shr_gridDomainPartitioningMethod_abs, only: shr_gridDomainPartitioningMethod
 
   use shr_gGrid_mod, only: shr_igGrid
   use shr_gridMask_mod, only: shr_gridMask_cast, shr_IgridMask, shr_gridMask
-  use shr_gridDomain_mod, only: shr_gridDomain, shr_iGridDomain
+  use shr_gridDomain_mod, only: shr_gridDomain, shr_iGridDomain, shr_gridDomainWrap
 
   use shr_gridMaskFindEnabledEqualSplitIterator_mod, only: shr_gridMaskFindEnabledEqualSplitIterator
   use shr_gridMaskFindEnabledEqualSplitMethod_mod, only: shr_gridMaskFindEnabledEqualSplitMethod
@@ -71,27 +72,61 @@ contains
     !< compute partitions
     class(shr_gridDomainPartitioningMethodBySameNumberOfGridcells), intent(inout) :: self
     class(shr_iGridDomain), intent(in) :: domain
-    class(shr_IgridMask), allocatable :: enabled
-    class(shr_iGridDomain), allocatable :: filteredDomain
-    class(shr_iggrid), allocatable :: filteredGrid
+    class(shr_IgridMask), allocatable :: enabled, enabledGMask
+    class(shr_iGridDomain), allocatable :: filteredDomain, selectedGridDomain
+    class(shr_iggrid), allocatable :: filteredGrid, shrinkedGrid
     integer :: isplit
-    type(shr_gridMask) :: tmpGMask
+    class(shr_igridMask), allocatable :: tmpGMask, tmpGMaskShrinked, tmpBorderMaskedShrinked
+!    type(string) :: tmp
 
+    allocate(filteredDomain, mold = domain) !< allocate and get its type
     allocate(self % gdomain, source = domain)
     allocate(self % newDomains(self % total), mold = domain)
     allocate(self % findEnabledEqualSplitMethod)
+
+    !tmp = self % gdomain % toString()
+    !write(*,*) "current domain =", tmp % toString()
 
     !< calculate earch part
     enabled = self % gdomain % getEnabledGridMask()
     call self % findEnabledEqualSplitMethod % init(self % total, enabled)
 
     !< over each part
+    !> 'extract' from given gridMask into new domain
+    !> with the same as gridMask as well as those
+    !> values marked in gridmask.
     do isplit = 1, self % findEnabledEqualSplitMethod % getSize()
-      !> convert each gridMask into new domain
+      !write(*,*) "gridDomainPart...:: calculate:: --------------------------------------------------- "
+      !write(*,*) "gridDomainPart...:: calculate:: isplit =", isplit
+      !< get calculated grid mask
       tmpGMask = self % findEnabledEqualSplitMethod % get(isplit)
+      !tmp = tmpGMask % toString()
+      !write(*,*) "tmpGMask =", tmp % toString()
+
+      !< find new grid shape
+      tmpGMaskShrinked = tmpGMask % shrink()
+      !< find new border
+      tmpBorderMaskedShrinked = .not. tmpGMaskShrinked
+      !tmp = tmpGMaskShrinked % toString()
+      !write(*,*) "tmpGMaskShrinked =", tmp % toString()
+
+      !< select enabled grid cell values from current domain
       filteredDomain = self % gdomain % filter(tmpGMask)
-      filteredGrid = filteredDomain % getGrid()
-      self % newDomains(isplit) = self % gdomain % select(filteredGrid)
+      !tmp = filteredDomain % toString()
+      !write(*,*) "gridDomainPart...:: calculate:: filteredDomain =", tmp % toString()
+
+      !< extract new grid shape
+      shrinkedGrid = tmpGMaskShrinked % getGrid()
+      !< find enabled grid cells values from domain
+      allocate(selectedGridDomain, source = filteredDomain % select(shrinkedGrid))
+      enabledGMask = selectedGridDomain % getEnabledGridMask()
+
+      !< build
+      call self % newDomains(isplit) % init(shrinkedGrid, enabledGMask, tmpBorderMaskedShrinked)
+
+      !< release
+      deallocate(selectedGridDomain)
+      deallocate(tmpGMask)
     end do
   end subroutine calculate
 
