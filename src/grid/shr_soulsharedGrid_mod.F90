f8f7f5c8c641d1a710d2e4cb160b5fcf29ad8679
ajornet <jurnix@gmail.com>
2022-02-22 15:36:47 +0100

    New: initialize shr_soulsharedGrid class

--- src/grid/shr_soulsharedGrid_mod.F90
+++ src/grid/shr_soulsharedGrid_mod.F90
@@ -8,7 +8,7 @@
 !
 ! DESCRIPTION:
 !>
-!> main soulshared_grid interface
+!> main soulshared grid interface
 !>
 !------------------------------------------------------------------------------
 module shr_soulsharedGrid_mod
@@ -17,41 +17,39 @@ module shr_soulsharedGrid_mod
   use shr_error_mod, only: raiseError
 
   use shr_gGrid_mod, only: shr_igGrid
-  use shr_gridPartition_mod, only: shr_gridPartition
   use shr_strings_mod, only: string
-  use shr_gridDomain_mod, only: shr_iGridDomain
+  use shr_gridDomain_mod, only: shr_iGridDomain, shr_gridDomainBuilder
 
   implicit none
 
   private
 
+  character(*), parameter :: SHR_GRID_TYPE_SQUARED = "squared"
+
   public :: shr_soulsharedGrid
 
   type shr_soulsharedGrid
-    class(shr_igGrid), allocatable :: grid
-    class(shr_igridDomain), allocatable :: domains(:) !< domain Collection?
-    type(string), allocatable :: type
-    type(shr_gridPartition), allocatable :: partitions
+    class(shr_igridDomain), allocatable :: gridDomain
   contains
     procedure :: init
+    !< combine
   end type shr_soulsharedGrid
 
 
 contains
 
-  subroutine init(self, resolution, bounds, type, partitions)
+  subroutine init(self, resolution, bounds, type)
     !< shr_soulsharedGrid initialization
     class(shr_soulsharedGrid), intent(inout) :: self
     real(kind=sp), intent(in) :: resolution
-    real(kind=sp), intent(in) :: bounds !< n, s, e, w
+    real(kind=sp), intent(in) :: bounds(4) !< n, s, e, w
     type(string), intent(in) :: type
-    type(shr_gridPartition), intent(in) :: partitions
 
-    ! structure
-    !type(shr_gridDomain) :: gloDomain
-    !type(shr_gridDomainPartitioning) :: gdPartitioning
-
-!    gloDomain = gridDomainFactory(resolution, bounds, type)
+    if (type == SHR_GRID_TYPE_SQUARED) then
+      self % gridDomain = shr_gridDomainBuilder(resolution, bounds)
+    else
+      call raiseError(__FILE__, "init", "Unknown/unsupported grid type")
+    end if
 
 !    call partitionMethod % init(current, total)
 !    call gdPartitioning % init(gloDomain, method=partitionMethod same Number of gridcells)
